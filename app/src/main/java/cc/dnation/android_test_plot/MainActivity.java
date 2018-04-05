da85d67691c8c9070b42c5173b00fad177849d29
ycwei982 <ycwei982@gmail.com>
2018-04-05 18:16:20 +0800

    Just push

--- app/src/main/java/cc/dnation/android_test_plot/MainActivity.java
+++ app/src/main/java/cc/dnation/android_test_plot/MainActivity.java
@@ -2,11 +2,19 @@ package cc.dnation.android_test_plot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +28,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+        public boolean onOptionsItemSelected(MenuItem item){
+            switch (item.getItemId()) {
+                case R.id.menu_btn1:
+                    Toast.makeText(this, "This is Button 1 in the menu", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.menu_btn2:
+                    Toast.makeText(this, "This is Button 2 in the menu", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+            }
+            return true;
+            }
+    }
